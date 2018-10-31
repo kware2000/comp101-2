0dc7a52199bdfbaf4e94278d7c5a26b43ea97ade
Karen <Karen@KarenPC>
2018-10-31 09:34:10 +0100

    Init create

---
+++ Comp101-1/src/Person.java
@@ -0,0 +1,8 @@
+
+public class Person {
+	float x = 10.0f;
+	public void walks() {
+		x--;
+		System.out.println("X: " + x);
+	}
+}
---
+++ Comp101-1/src/Player.java
@@ -0,0 +1,10 @@
+
+public class Player {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		Person tom = new Person();
+ 		tom.walks();
+	}
+
+}
