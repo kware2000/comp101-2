6cec0e62e5de5a95168194ea98200dc7db1128f2
Karen <Karen@KarenPC>
2018-10-31 09:48:29 +0100

    Add getters and setters

--- Comp101-1/src/Person.java
+++ Comp101-1/src/Person.java
@@ -7,4 +7,22 @@ public class Person {
 		x--;
 		System.out.println("X: " + x);
 	}
+	public float getX() {
+		return x;
+	}
+	public void setX(float x) {
+		this.x = x;
+	}
+	public float getHeight() {
+		return height;
+	}
+	public void setHeight(float height) {
+		this.height = height;
+	}
+	public float getWeight() {
+		return weight;
+	}
+	public void setWeight(float weight) {
+		this.weight = weight;
+	}
 }
