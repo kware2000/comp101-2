b7244649f49173108368ba52877b61d235173a1b
Karen <Karen@KarenPC>
2018-10-31 15:19:03 +0100

    addeded Villian

--- Comp101-2/src/com/mycompany/comp101/Hero.java
+++ Comp101-2/src/com/mycompany/comp101/Hero.java
@@ -2,5 +2,13 @@ package com.mycompany.comp101;
 
 public class Hero {
 	public float strength;
+	String name;
 	
+	public Hero(String name) {
+		this.name = name;
+	}
+	
+	public void go() {
+		System.out.println(name + " is going.");
+	}
 }
--- Comp101-2/src/com/mycompany/comp101/Project.java
+++ Comp101-2/src/com/mycompany/comp101/Project.java
@@ -1,5 +1,11 @@
 package com.mycompany.comp101;
 
 public class Project {
-
+	
+	public static void main(String[] args) {
+		Hero h = new Hero("Tom Hero");
+		Villian v = new Villian("Victor Villian");
+		h.go();
+		v.go();
+	}
 }
